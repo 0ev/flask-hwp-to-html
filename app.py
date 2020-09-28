94e1f1d890ada09dff0a7d7a04b4342cffbe72a0
andy ye <andyye.jongcye@gmail.com>
2020-09-28 09:35:05 +0900

    andyye

--- app.py
+++ app.py
@@ -3,8 +3,19 @@ from werkzeug.utils import secure_filename
 from hwptohtml import change_to_html
 import os
 import stat
-st = os.stat('static/hwp5html.exe')
-os.chmod('static/hwp5html.exe', st.st_mode | stat.S_IEXEC)
+
+current_path=os.path.dirname(__file__)
+
+print(current_path)
+
+static_path=os.path.join(current_path,'static')
+
+print(static_path)
+
+temp_path=os.path.join(static_path,'tempfile')
+
+st = os.stat(os.path.join(static_path,'hwp5html.exe'))
+os.chmod(os.path.join(static_path,'hwp5html.exe'), st.st_mode | stat.S_IEXEC)
 
 ALLOWED_EXTENSIONS = {'hwp'}
 
@@ -12,7 +23,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-app = Flask(__name__,static_folder='static')
+app = Flask(__name__)
 @app.route('/upload')
 def render_file():
     return render_template('uploading.html')
@@ -23,11 +34,11 @@ def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         if allowed_file(f.filename):
-            f.save('static/tempfile/'+secure_filename(f.filename))
+            f.save(os.path.join(temp_path,secure_filename(f.filename)))
 
-            a=change_to_html('static/tempfile/'+secure_filename(f.filename))
+            a=change_to_html(os.path.join(temp_path,secure_filename(f.filename)))
 
-            return render_template_string(open("static/tempfile/"+a+"/result.html",'r',encoding='UTF8').read())
+            return render_template_string(open(os.path.join(temp_path,a,"result.html"),'r',encoding='UTF8').read())
         else:
             return render_template('uploading.html')
 
--- hwptohtml.py
+++ hwptohtml.py
@@ -3,6 +3,16 @@ import uuid
 import os
 import bs4
 
+current_path=os.path.dirname(__file__)
+
+print(current_path)
+
+static_path=os.path.join(current_path,'static')
+
+print(static_path)
+
+temp_path=os.path.join(static_path,'tempfile')
+
 def combine(path):
     soup = bs4.BeautifulSoup(open(path+"/hey.html",'r',encoding='UTF8').read(),features="lxml")
     stylesheets = soup.findAll("link", {"rel": "stylesheet"})
@@ -20,7 +30,7 @@ def set_img(path,id):
 
     for img in soup.findAll('img'):
         try:
-            img['src'] = r"{{url_for('" + '/static' + r"', filename='tempfile/" + id + '/' +img['src'] + r"')}}"
+            img['src'] = r"{{url_for('" + 'static' + r"', filename='tempfile/" + id + '/' +img['src'] + r"')}}"
         except:
             pass
     open(path+"/result.html", "w",encoding='UTF8').write(str(soup))
@@ -30,15 +40,16 @@ def change_to_html(path):
 
     id=str(uuid.uuid4())
 
-    os.mkdir('/static/tempfile/'+id)
+    os.mkdir(os.path.join(temp_path,id))
 
-    subprocess.check_call(['/static/hwp5html.exe',"--output", "/static/tempfile/" + id , path])
+    subprocess.check_call([os.path.join(static_path,'hwp5html.exe'),"--output", os.path.join(temp_path, id) , path])
 
-    subprocess.check_call(['/static/hwp5html.exe',"--html","--output", "/static/tempfile/"+id+"/hey.html",path])
+    subprocess.check_call([os.path.join(static_path,'hwp5html.exe'),"--html","--output", os.path.join(temp_path,id,"hey.html"),path])
 
-    combine("/static/tempfile/"+id)
+    combine(os.path.join(temp_path,id))
 
-    set_img("/static/tempfile/"+id,id)
+    set_img(os.path.join(temp_path,id),id)
+
+    print(id)
 
     return id
-
